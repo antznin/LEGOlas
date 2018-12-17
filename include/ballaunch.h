91e47dff6ab2741f9a14a19c407c42af6e9a993d
antznin <godard@eurecom.fr>
2018-12-17 17:44:53 +0100

    Integrate ballaunch.c into project

---
+++ include/ballaunch.h
@@ -0,0 +1,12 @@
+#ifndef BALLAUNCH_H_
+#define BALLAUNCH_H_
+
+void catch_ball();
+void place_hand_for_movement();
+void dismiss_hand_nc();
+void dismiss_hand_wc();
+void throw_ball();
+void hold_catapult_for_movement();
+void release_catapult();
+
+#endif
