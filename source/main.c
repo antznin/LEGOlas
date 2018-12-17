b400b7455d6d6558f861b6ca178439b84268fd45
Yasmine Bennani <yasmine.bennani@eurecom.fr>
2018-12-17 10:53:22 +0100

    New main function which does the entire scan

--- source/main.c
+++ source/main.c
@@ -17,95 +17,106 @@
 #define Sleep( msec ) usleep(( msec ) * 1000 )
 #define ARM_LENGTH 10 /* Length of the robot's arm that will catch the ball */
 
+int total_scan(int angle, int radius, int step_id){
+    struct values scan_values;
+    int sign, ball_found;
+
+    if (angle > 0) {
+        sign = 1;
+    } else {
+        sign = -1;
+    }
+
+    scan_values = scan(angle, radius);
+    if(scan_values.angle != 0.0 && scan_values.radius != 0.0){
+        ball_found = move_to_ball((int)scan_values.radius); //To be able to catch the ball the robot must be 5cm away from it
+        if(ball_found) {
+           // TO ADD : Sami's functions
+            move_forward( - (int)(scan_values.radius - ARM_LENGTH));
+            switch (step_id){
+                case 1: turn(-(sign * scan_values.angle));
+                case 2: turn(- (55.0 + scan_values.angle));
+                case 3: turn(55 - scan_values.angle);
+            }
+            printf("Ball found! \n");
+            return 1;
+        }
+        else {
+            printf("No ball to catch \n");
+            move_forward(-(int)(scan_values.radius + 5));
+            turn(sign * (angle - scan_values.angle));
+            return 0;
+        }
+    }
+    return 0;
+}
 
 /* Scan the entire field in 5 zones to find the ball and move to it */
 /*            Returns 0 if no ball found, 1 otherwise               */
 
 int explore(){
-    struct values scan0;
-    struct values scan1;
-    struct values scan2;
-    struct values scan3;
-    struct values scan4;
-    struct values scan5;
-    struct values scan6;
-    struct values scan7;
-    
-    /* Fisrt scans : see if the ball is somewhere around */
-    scan0 = scan(55, 20);
-    if(scan0.angle != 0.0 && scan0.radius != 0.0){
-        move_forward((scan0.radius - ARM_LENGTH)); //To be able to catch the ball the robot must be 5cm away from it
-        printf("Ball found! \n");
+    int res1;
+    int res2;
+    int res3;
+    int res4;
+    int res5;
+    int res6;
+    int res7;
+    int res8;
+
+    /* Fisrt scans : see if the ball is somewhere around (STAGE 1)*/
+    res1 = total_scan(55, 20, 1);
+    if(res1)
         return 1;
-    }
-    scan1 = scan(250, 50);
-    if(scan1.angle != 0.0 && scan1.radius != 0.0){
-        move_forward((scan1.radius - ARM_LENGTH));
-        printf("Ball found! \n");
+
+    res2 = total_scan(250, 30, 2);
+    if(res2)
         return 1;
-    }
-    scan2 = scan(55, 20);
-    if(scan2.angle != 0.0 && scan2.radius != 0.0){
-        move_forward((scan2.radius - ARM_LENGTH)); //To be able to catch the ball the robot must be 5cm away from it
-        printf("Ball found! \n");
+
+    res3 = total_scan(55, 20, 3);
+    if(res3)
         return 1;
-    }
-    
+
     move_forward(15);
     turn(-90);
-    
+
     /* Fourth scan to know if it can move forward without kicking the ball */
     turn(-10);
-    scan3 = scan(10, 50);
-    if(scan3.angle != 0.0 && scan3.radius != 0.0){
-        move_forward((scan3.radius - ARM_LENGTH));
-        printf("Ball found! \n");
+    res4 = total_scan(10, 50, 1);
+    if(res4)
         return 1;
-    }
-    
-    /* Go to stage2 and scan */
+
+    /* Go to STAGE 2 and scan */
     move_forward(38);
     turn(-90);
-    scan4 = scan(-80, 40);
-    if(scan4.angle != 0.0 && scan4.radius != 0.0){
-        move_forward((scan4.radius - ARM_LENGTH));
-        printf("Ball found! \n");
-        return 1;
-    }
+    res5 = total_scan(-80, 50, 1);
+    if(res5)
+        return 2;
     turn(80);
-    
-    /* Go to stage3 and scan */
+
+    /* Go to STAGE 3 and scan */
     move_forward(80);
     turn(90);
-    scan5 = scan(-80, 40);
-    if(scan5.angle != 0.0 && scan5.radius != 0.0){
-        move_forward((scan5.radius - ARM_LENGTH));
-        printf("Ball found! \n");
-        return 1;
-    }
+    res6 = total_scan(-80, 50, 1);
+    if(res6)
+        return 3;
     turn(80);
-    
-    /* Go to stage4 and scan */
+
+    /* Go to STAGE 4 and scan */
     move_forward(80);
     turn(-90);
-    scan6 = scan(-80, 60);
-    if(scan5.angle != 0.0 && scan5.radius != 0.0){
-        move_forward((scan5.radius - ARM_LENGTH));
-        printf("Ball found! \n");
-        return 1;
-    }
+    res7 = total_scan(-80, 50, 1);
+    if(res7)
+        return 4;
     turn(80);
-    
-    /* Go to stage5 and scan */
+
+    /* Go to STAGE 5 and scan */
     move_forward(80);
     turn(-90);
-    scan7 = scan(-80, 60);
-    if(scan7.angle != 0.0 && scan7.radius != 0.0){
-        move_forward((scan7.radius - ARM_LENGTH));
-        printf("Ball found! \n");
-        return 1;
-    }
-    
+    res8 = total_scan(-80, 50, 1);
+    if(res8)
+        return 5;
+
     printf("Didn't find any ball!\n");
     return 0;
 }
@@ -118,13 +129,19 @@ int main(){
         printf("Failed to init \n");
         return -1;
     }
-    
+
     int ball_found = explore();
     if(ball_found){
-        
+        switch (ball_found){
+            case 1: shoot_from_stage1();
+            case 2: shoot_from_stage2();
+            case 3: shoot_from_stage3();
+            case 4: shoot_from_stage4();
+            case 5: shoot_from_stage5();
+        }
     }
     else{
-        
+        printf("NO BALL FIND: EXITED \n");
     }
     exit_robot();
 }
--- source/movement.c
+++ source/movement.c
@@ -13,6 +13,8 @@
 #include "ev3_port.h"
 #include "ev3_tacho.h"
 #include "ev3_sensor.h"
+#include "ballDetection.h"
+#include <time.h>
 
 #define Sleep( msec ) usleep(( msec ) * 1000 )
 #define PI 3.1415
@@ -76,6 +78,75 @@ void go_to_corner(int to_right){
     }
 }
 
+
+/***********************************  MOVE TO BALL ******************************/
+/* Move when the scan has found a ball, and verify that the ball is really here */
+
+int move_to_ball(int dist){ /* dist is the distance returned by the scan function = the presumed distance of the ball */
+    uint8_t sn_left, sn_right, sn_us;
+    int port_left = 66; /* Left wheel */
+    int port_right = 67; /* Right wheel */
+    int max_speed_right, max_speed_left, max_speed, speed, count_per_rot;
+    int inf = dist - 5; /* The robot must find the ball between dist - 10 and dist + 10 cm */
+    int sup = dist + 5;
+    float value, init, t;
+    int ball_found = 0;
+    long int init_time, current_time;
+
+    if(ev3_search_sensor(LEGO_EV3_US, &sn_us, 0))
+        get_sensor_value0(sn_us, &init);
+    else
+        printf("[x] US sensor not found \n");
+
+    if( ev3_search_tacho_plugged_in(port_left, 0, &sn_left, 0) &&  ev3_search_tacho_plugged_in(port_right,0, &sn_right, 0 )) {
+        get_tacho_max_speed(sn_left, &max_speed_left);
+        get_tacho_max_speed(sn_right, &max_speed_right);
+        get_tacho_count_per_rot(sn_left, &count_per_rot);
+
+        if(max_speed_left == max_speed_right || max_speed_left < max_speed_right)  /* Set max_speed to the lowest max_speed (left or right) */
+                max_speed = max_speed_left;
+            else
+                max_speed = max_speed_right;
+
+        set_tacho_stop_action_inx(sn_left, TACHO_COAST);
+        set_tacho_stop_action_inx(sn_right, TACHO_COAST);
+
+        speed = max_speed * 1/10;
+        set_tacho_speed_sp(sn_left, speed);
+        set_tacho_speed_sp(sn_right, speed);
+
+        /* Calculate the maximum time the tacho can run */
+        t = (float) (count_per_rot * sup )/(speed * PI * WHEEL_DIAM) ;
+
+        /* Set tachos to run for t seconds */
+        set_tacho_time_sp( sn_left, t*1000 );
+        set_tacho_time_sp( sn_right, t*1000 );
+        set_tacho_command_inx( sn_left, TACHO_RUN_TIMED );
+        set_tacho_command_inx( sn_right, TACHO_RUN_TIMED );
+        init_time = (long int) time(NULL); // Current time in seconds
+        current_time = (long int) time(NULL); //idem
+
+        get_sensor_value0(sn_us, &value);
+        printf("US VALUE: %f\n", value);
+        while(value > 100 && (current_time - init_time) < (long int) t){
+            get_sensor_value0(sn_us, &value);
+            current_time = (long int) time(NULL);
+            printf("US VALUE : %f, TIME: %ld \n", value, current_time);
+        }
+
+        if((current_time - init_time) < (long int) t)
+            ball_found = 1;
+
+        printf("Exited the while loop with ball_found = %d\n", ball_found);
+
+        set_tacho_command_inx(sn_left, TACHO_STOP);
+        set_tacho_command_inx(sn_right, TACHO_STOP);
+    }
+    else
+       printf("[x] Tachos not found \n");
+
+    return ball_found;
+}
 
 void turn(float angle) {
     uint8_t sn_left, sn_right, sn_compass;
@@ -255,3 +326,5 @@ int exit_robot(void){ //Exit the ev3
     printf( "*** ( EV3 ) Bye! ***\n" );
     return ( 0 );
 }
+
+
