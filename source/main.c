09fa0ac0186b1ec799dece030dc05a94513b2cc4
Yasmine Bennani <yasmine.bennani@eurecom.fr>
2019-01-21 17:54:55 +0100

    Main version for th 22th January: 2 strategies, bluetooth handler missing

--- source/main.c
+++ source/main.c
@@ -1,12 +1,5 @@
 /**************************************************************************************************/
-<<<<<<< HEAD
 /************************      MAIN FUNCTION FOR THE 22th JANUARY     *****************************/
-/************************         Written by Yasmine Bennani          *****************************/
-=======
-/************************     MAIN FUNCTION FOR THE 18th DECEMBER     *****************************/
-/************************         Written by                 *****************************/
->>>>>>> ballDetection
-/**************************************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,6 +15,7 @@
 #include "dead_reckoning.h"
 
 #define Sleep( msec ) usleep(( msec ) * 1000 )
+#define DELAY 1000
 #define ARM_LENGTH 8 /* Length of the robot's arm that will catch the ball */
 
 
@@ -31,11 +25,12 @@
 
 void strategy1(){
     /* Throw the first two balls */
+    move_forward(-8);
+    Sleep(1000);
     throw_ball();
-    Sleep(500);
-    catch_ball();
-    dismiss_hand_nc();
-    Sleep(500);
+    dismiss_hand_wc();
+    catch_ball(1600);
+    dismiss_hand_wc();
     throw_ball();
     
     /* Go to the opponent's field */
@@ -49,19 +44,63 @@ void strategy1(){
 /*   Throw the first two balls and then scan to look for the ball  */
 /*******************************************************************/
 
-void stratey2(){
+void strategy2(){
+    struct values first_scan_values;
+    struct values second_scan_values;
+    struct values ball_position;
+    int x,y;
+    int found = 0;
+    int count = 0;
+    
     /* Throw the first two balls */
+    move_forward(-8);
+    Sleep(1000);
     throw_ball();
-    Sleep(500);
-    catch_ball();
-    dismiss_hand_nc();
-    Sleep(500);
+    dismiss_hand_wc();
+    catch_ball(1600);
+    dismiss_hand_wc();
     throw_ball();
+    
+    /* Move to scan position */
+    move_to_xy(-6,9);
+    init_orientation();
     
     /* Then scan, find a ball, go to shoot position, shoot and scan again */
-    
+    while(1){
+        first_scan_values = single_scan(DELAY,1,45,1, 360);
+        second_scan_values = single_scan(DELAY,2,45,-1, first_scan_values.angle);
+        count += 1;
+        
+        if (are_close(first_scan_values,second_scan_values)){
+            ball_position.radius = (first_scan_values.radius + second_scan_values.radius)/2;
+            ball_position.angle = (first_scan_values.angle + second_scan_values.angle)/2;
+            printf("Ball is at radius %f , angle %f ", ball_position.radius,ball_position.angle);
+            turn(ball_position.angle);
+            release_catapult();
+            move_forward((int)(ball_position.radius/10) - ARM_LENGTH);
+            catch_ball(1600);
+            /* Go to shoot position */
+            move_to_xy(0,0);
+            move_forward(-8);
+            init_orientation();
+            dismiss_hand_wc();
+            throw_ball();
+        }
+        
+        if((count % 3) == 1){
+            move_to_xy(-6,9);
+            init_orientation();
+        }
+        else if((count % 3) == 2){
+            move_to_xy(-10, -9);
+            init_orientation();
+        }
+        else if((count % 3) == 0){
+            move_to_xy(10, -9);
+            init_orientation();
+        }
+    }
 }
-
 
 
 int main(){
@@ -83,56 +122,4 @@ int main(){
     exit_robot();
 }
 
-// #define ARM_LENGTH 20 /* Length of the robot's arm that will catch the ball */
-// 
-// #define DELAY 1000
-// 
-// int main(){
-// 	int is_init;
-//     	/* Check for the right initialization of the robot */
-//     	is_init = init_robot();
-//     	if(is_init == -1){
-//     	    printf("Failed to init \n");
-//     	    return -1;
-//     	}
-//     	
-//     	// Arm initial movement
-// 	move_forward(-9);
-// 	Sleep(1500);
-//     	throw_ball();
-//     	Sleep(1000);
-//     	catch_ball(600);
-//     	dismiss_hand_wc();
-//     	throw_ball();
-//     	Sleep(1000);
-//     	hold_catapult_for_movement();
-// 
-// 
-//     	/* execute 2 scans and get relevant values i.e values that are close from each other */
-// 	struct values first_scan_values;
-// 	struct values second_scan_values;
-// 	first_scan_values = single_scan(DELAY,1,45,1, 360);
-// 	second_scan_values = single_scan(DELAY,2,45,-1, first_scan_values.angle);
-// 	struct values ball_position;
-// 	int x,y;
-// 	int found = 0;
-// 	
-// 
-// 
-// 
-// 	// for (x=0 ; x<5 && not_found; x++){
-// 	// 	for (y=0; y<5 ; y++){
-// 	if (are_close(first_scan_values,second_scan_values)){
-// 		ball_position.radius = (first_scan_values.radius + second_scan_values.radius)/2;
-// 		ball_position.angle = (first_scan_values.angle + second_scan_values.angle)/2;
-// 		printf("Ball is at radius %f , angle %f ", ball_position.radius,ball_position.angle);	
-// 		turn(ball_position.angle);
-// 		release_catapult();
-// 		move_forward((int)(ball_position.radius/10) - ARM_LENGTH);
-// 		catch_ball(1600);
-// 		dismiss_hand_wc();
-// 		throw_ball();
-// 	}
-// 	release_catapult();
-// 	// 	}
-// 	// }
+
